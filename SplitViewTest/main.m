5d20543effb6aacda83423e448bc2c9cfdfbdbcb
Saori Kohno <saorikohno@yahoo.com>
2016-03-01 06:22:33 +0900

    Initial Commit

---
+++ SplitViewTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SplitViewTest
+//
+//  Created by 河野 さおり on 2016/03/01.
+//  Copyright © 2016年 河野 さおり. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
